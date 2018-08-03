00b1fab86abab9be2ee93d9a1a938e0224b03d57
Oliver Snowden <oliversnowden@snodnipper.co.uk>
2018-08-03 21:25:35 +0100

    add geovation centre

--- data/run.sh
+++ data/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 tippecanoe --no-tile-compression --layer="dawgs" --minimum-zoom=0 --maximum-zoom=14 --base-zoom=0 -e "dawgs" "dawgs.geojson"
-cp config.json ./dawgs
+tippecanoe --no-tile-compression --layer="geovation" --minimum-zoom=0 --maximum-zoom=14 --base-zoom=0 -e "geovation" "geovation.geojson"
+cp config.json.dawg ./dawgs/config.json
+cp config.json.geovation ./geovation/config.json
 
---
+++ run-sprites.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if ! type "docker" > /dev/null; then
+  exit
+fi
+
+docker run -it -e FOLDER=_svg -v ${PWD}:/data dolomate/spritezero
