70cd54dc4f11d9b79d9590a623648f6d12ba47e7
Oliver Snowden <oliversnowden@snodnipper.co.uk>
2018-08-03 18:30:14 +0100

    add style

--- data/run.sh
+++ data/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-tippecanoe --layer="dawgs" --minimum-zoom=0 --maximum-zoom=14 --base-zoom=0 -e "dawgs" "dawgs.geojson"
+tippecanoe --no-tile-compression --layer="dawgs" --minimum-zoom=0 --maximum-zoom=14 --base-zoom=0 -e "dawgs" "dawgs.geojson"
 cp config.json ./dawgs
 
